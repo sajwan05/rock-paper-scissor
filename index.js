efb0e4f732aa91a5ab84087046e48f31c357a3e6
sajwan05 <surajsajwan328@gmail.com>
2026-02-21 21:22:17 +0530

    Write logic for playround function and initialize scores

--- index.js
+++ index.js
@@ -15,3 +15,36 @@ function getHumanChoice() {
     return humanChoice.toLowerCase().trim();
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(yourChoice, botChoice){
+    if((yourChoice === "rock" && botChoice === "scissors") ||
+       (yourChoice === "paper" && botChoice === "rock") ||
+       (yourChoice === "scissors" && botChoice === "paper")) {
+        humanScore++;
+        console.log(`Hurrah! You won this Round.
+            You Chose: ${yourChoice} BEATS Bot Choice: ${botChoice} 
+            Score: You: ${humanScore}---- Bot: ${computerScore}`);
+       }else if((botChoice === "rock" && yourChoice === "scissors") ||
+                 (botChoice === "scissors" && yourChoice === "paper") ||
+                 (botChoice === "paper" && yourChoice === "rock")) {
+                    computerScore++;
+                    console.log(`Sad! You lost this Round.
+            Bot Choice: ${botChoice} beats yourChoice: ${yourChoice}
+            Score: You: ${humanScore}---- Bot: ${computerScore}`);
+
+        } else {
+            computerScore++;
+            humanScore++;
+            console.log(`Alas! This Round got Tied. 
+            Both of you chose: ${botChoice} 
+            Score: You: ${humanScore}---- Bot: ${computerScore}`);
+        }
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+playRound(humanChoice, computerChoice);
+
